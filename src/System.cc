7cfe3dc34131feb966a64555a399b14d651f0381
jeongmyunglee <leeejeongmyung@gmail.com>
2022-06-01 00:54:23 +0900

    pcd header

--- src/System.cc
+++ src/System.cc
@@ -502,6 +502,7 @@ void System::SaveMapPoints(const string &filename){
 	ofstream f;
 	f.open(filename.c_str());
 	f << fixed;
+	f <<  "# .PCD v.7 - Point Cloud Data file format\nVERSION .7\nFIELDS x y z\nSIZE 4 4 4\nTYPE F F F\nCOUNT 1 1 1\nWIDTH 94645\nHEIGHT 1\nVIEWPOINT 0 0 0 1 0 0 0\nPOINTS 94645\nDATA ascii\n";
 
 	for(size_t i=0; i<vpMPs.size(); i++) {
 		MapPoint* pMP = vpMPs[i];
